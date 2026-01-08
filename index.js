31ec68b5f8ac3d2e3bc74a26faa1537df0130497
Your Name <your-email@example.com>
2026-01-07 20:54:34 -0800

    if statement for Format

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const express = require('express');
+import express from 'express';
+import BOOKS from './books.js';
+
+const app = express();
+const port = 3000;
+
+app.get('/books', (req, res) => {
+  console.log(req.query);
+  // res.json(BOOKS);
+  let response = [];
+
+  for (const book of BOOKS) {
+    // response.push(book);
+    // no query than push(book)
+    // else
+    // if we have a format === book.format push(format)
+    if (Object.keys(req.query).length === 0) {
+      response.push(book);
+    } else {
+      if (req.query['format'] === book['format']) {
+        response.push(book);
+      }
+    }
+  }
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`serveris lisening on port ${port}`);
+});
